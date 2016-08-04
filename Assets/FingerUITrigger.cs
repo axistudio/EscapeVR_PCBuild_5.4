7a2ac9893a56e6f37f9fd322a8fac8a1896f575d
axistudio <caoyuxi@me.com>
2016-08-03 20:33:32 -0400

    finger trigger
    finger trigger

---
+++ Assets/FingerUITrigger.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FingerUITrigger : MonoBehaviour
+{
+	Transform finger;
+	public GameObject currentPage;
+	public GameObject nextPage;
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		print (other);
+		if (other.gameObject.layer == 8 && !finger) {
+			finger = other.transform;
+			buttonPressed ();
+		}
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		if (other.gameObject.layer == 8) {
+			finger = null;
+		}
+	}
+
+	void buttonPressed ()
+	{
+		currentPage.SetActive (false);
+		nextPage.SetActive (true);
+	}
+}
---
+++ Assets/FingerUITrigger_Scroll.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FingerUITrigger_Scroll : MonoBehaviour
+{
+	public float minS;
+	public float maxS;
+
+	float oldFingerS;
+	Transform finger;
+	// Use this for initialization
+	void Start ()
+	{
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (finger) {
+			//Pressed
+			float f = finger.position.y - oldFingerS;
+			if (f != 0) {
+				transform.position += new Vector3 (0, f, 0);
+				oldFingerS = finger.position.y;
+			}
+		}
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		print (other);
+		if (other.gameObject.layer == 8 && !finger) {
+			finger = other.transform;
+			oldFingerS = finger.position.y;
+		}
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		if (other.gameObject.layer == 8) {
+			finger = null;
+		}
+	}
+}
