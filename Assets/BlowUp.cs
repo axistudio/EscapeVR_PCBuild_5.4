1e05b20e7c9b06e892c5c91524588bbe669ce07d
axistudio <caoyuxi@me.com>
2016-08-20 16:07:45 -0700

    Revert "no_exploder"
    This reverts commit 3c9f3cd26ea0233b061fdec7aaed7b3f4170c59c.

---
+++ Assets/BlowUp.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using Exploder;
+
+public class BlowUp : MonoBehaviour {
+
+	public ExploderObject exploder;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		exploder.ExplodeRadius ();
+	}
+}
