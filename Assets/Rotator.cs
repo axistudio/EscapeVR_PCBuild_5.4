56c17c57a5886a1eac49d73ebf563f2d5727b51c
HelixNGC7293 <whu1@sva.edu>
2016-08-20 03:11:38 -0400

    For Trailer

---
+++ Assets/Rotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	public Vector3 v3;
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (v3 * Time.deltaTime);
+	}
+}
---
+++ Assets/TextTimer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextTimer : MonoBehaviour {
+	TextMesh textTimer;
+	public float secondsLeft = 60;
+	// Use this for initialization
+	void Start () {
+		textTimer = GetComponent<TextMesh> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (secondsLeft > 0) {
+			secondsLeft -= Time.deltaTime;
+			textTimer.text = TransferTimer (secondsLeft); 
+		} else {
+			//Over
+			textTimer.text = "00:00"; 
+		}
+	}
+
+	public string TransferTimer(float t){
+		string s = "";
+		int tCheck = 0;
+		//Timer
+		tCheck = Mathf.FloorToInt(t);
+		if (tCheck < 10) {
+			s += "0" + tCheck;
+		} else {
+			s += tCheck;
+		}
+		s += ":" ;
+		//0-9
+		tCheck = Mathf.FloorToInt ((t - Mathf.FloorToInt (t)) * 100);
+		if (tCheck < 10) {
+			s += "0" + tCheck;
+		} else {
+			s += tCheck;
+		}
+		return s;
+	}
+}
